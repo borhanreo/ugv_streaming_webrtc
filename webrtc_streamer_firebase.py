1cd0387ca63b758ebb4f32cef61d0b8e56971d65
borhan uddin <borhan.uddin@bjitgroup.com>
2026-05-21 10:52:51 +0600

    No video latency new code added

--- webrtc_streamer_firebase.py
+++ webrtc_streamer_firebase.py
@@ -109,7 +109,7 @@ async def main(room_id: str):
     # player = MediaPlayer("/dev/video0", format="v4l2", options={"video_size": "320x240","input_format": "yuyv422", "framerate": "10"})
     # pc.addTrack(player.video)
     player = MediaPlayer("/dev/video0", format="v4l2", options={"framerate": "15"})
-    scaled = ResizedVideoTrack(player.video, 640, 480)
+    scaled = ResizedVideoTrack(player.video, 320, 240)
     pc.addTrack(scaled)
 
     if is_callee:
