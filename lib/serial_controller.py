6707be1e715c09bc26ff21633cbaa1588901a5ec
borhan uddin <borhan.uddin@bjitgroup.com>
2026-06-04 09:31:27 +0600

    motordriver added serial by arduino

--- lib/serial_controller.py
+++ lib/serial_controller.py
@@ -164,14 +164,8 @@ class SerialController:
                 self.send_motor_command("M4", "R", 0)
 
             case Constant.MQTT_T_VAL_SERVO_ROTATE_TEST:
-    
-                for i in range(0, 181):      # 181 because the stop value is exclusive
-                    print(i)
-                    self.send_servo_command("S2", i)
-                    sleep(0.1)  # Optional: add a small delay between commands
-                # Count down from 180 to 0
-                for i in range(180, -1, -1): # step -1 to go backwards
-                    self.send_servo_command("S2", i) 
-                    sleep(0.1)  # Optional: add a small delay between commands
+                logger.info("MQTT → Servo Rotate Test")
+                self.send_servo_command("S2", speed) 
+                   
             case _:
                 logger.debug("MQTT → unhandled command t=%s", t)
