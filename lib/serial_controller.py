b9d3952cbb340999b5a4cc141069176e58ef7639
borhan uddin <borhan.uddin@bjitgroup.com>
2026-06-05 11:28:45 +0600

    issue fix for MQTT restat command

--- lib/serial_controller.py
+++ lib/serial_controller.py
@@ -168,9 +168,9 @@ class SerialController:
                 self.send_servo_command("S2", speed) 
             case Constant.MQTT_T_VAL_RPI_RESTART:
                 logger.info("MQTT → RPi Restart")
-                device_restart()
+                #device_restart()
             case Constant.MQTT_T_VAL_RPI_SHUTDOWN:
                 logger.info("MQTT → RPi Shutdown")
-                device_shutdown()
+                #device_shutdown()
             case _:
                 logger.debug("MQTT → unhandled command t=%s", t)
