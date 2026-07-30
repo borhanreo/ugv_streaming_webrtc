802e69d8bd652ca77860d9840728476f4f415a59
borhan uddin <borhan.uddin@bjitgroup.com>
2026-07-30 14:12:46 +0600

    add turn condition

--- main.py
+++ main.py
@@ -259,10 +259,9 @@ DEFAULT_ICE_SERVERS = [
     RTCIceServer(
         urls=["turn:103.197.206.61:3478?transport=udp", "turn:103.197.206.61:3478?transport=tcp"],
         username="turnuser",
-        credential="Aa12341!"
+        credential="Aa12341!" ##password
     )
 ]
-
 
 def _coerce_int(value):
     if value is None:
